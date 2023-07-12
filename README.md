216de0a2cd6613ed5b4bdcd4b0a29842c33e2254
Max Cobb <maxxc@mac.com>
2023-07-12 11:34:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# agora-activefence-kicker
+
+This Go package provides a library for kicking users from Agora channels via the REST API. If used directly with the cmd/main.go file it will expect the same POST request that the ActiveFence backend sends at the "/kick" endpoint.
+
+## Usage
+
+To use this package, set the following environment variables:
+
+- `APP_ID` - Your Agora app ID  
+- `CUSTOMER_KEY` - Your Agora customer key
+- `CUSTOMER_SECRET` - Your Agora customer secret
+
+You can optionally add these in a `.env` file at the root directory of the project. 
+
+Then import the package and call the `KickUser` method:
+
+```go
+import "github.com/myorg/agora-activefence-kicker"
+
+err := kicker.KickUser(channelName, uid, durationMinutes) 
+```
+
+This will call the Agora API to ban the user from joining the specified channel for the duration. 
+
+See the [examples](/examples) folder for more usage examples.
+
+## Running 
+
+To run the example server:
+
+```
+go run cmd/main.go
+```
+
+This will start a basic web server that exposes a `/kick` endpoint for kicking users.
+
+## ActiveFence Webhooks
+
+An example of the POST data from ActiveFence includes:
+
+```json
+{
+  "contentUrl": "s3://activefence-content/temp/rehosted/agora-demo/agore-demo/ba5be0b6-6867-411b-b9c2-4d4e01b62b66",
+  "status": "61c97b8b9abfed64af087e22",
+  "userId": "556544384",
+  "addedAt": "2023-07-11T17:46:47.000Z",
+  "contentId": "D73F290CE6EC4E038E84E2BE52687109",
+  "reason": [
+    "Unauthorised Sales"
+  ],
+  "metadata": "{\"callbackData\":\"\",\"cname\":\"test3\",\"requestId\":\"D73F290CE6EC4E038E84E2BE52687109\",\"sid\":\"7921EE8B14674DA48B1C690BAB3758B8\",\"source\":\"agora\",\"timestamp\":20230711174617885,\"uid\":556544384}"
+}
+```
+
+The minimum required from ActiveFence would be userId, and metadata.
+
+## License
+
+This project is licensed under the MIT license. See [LICENSE](/LICENSE) for details.
---
+++ service/handlers.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Service) kickUser(c *gin.Context) {
+
+	var incomingReq ActiveFenceReq
+	json.NewDecoder(c.Request.Body).Decode(&incomingReq)
+
+	// Parse metadata
+	var md ReqMetadata
+	fmt.Println(incomingReq.Metadata)
+	err := json.Unmarshal([]byte(incomingReq.Metadata), &md)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid Request: " + err.Error(),
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+	channel := md.Cname
+
+	// Call Agora API to kick
+	url := "https://api.agora.io/dev/v1/kicking-rule"
+
+	data := map[string]interface{}{
+		"appid":           s.appID,
+		"cname":           channel,
+		"uid":             incomingReq.UID,
+		"time_in_seconds": 300, // 5 minute ban
+		"privileges":      []string{"join_channel"},
+	}
+	jsonData, _ := json.Marshal(data)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Error: " + err.Error(),
+			"status":  http.StatusInternalServerError,
+		})
+		return
+	}
+	// Add Authorization header
+	req.Header.Add("Authorization", "Basic "+s.restuflToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send HTTP request
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Request to Agora API Failed: " + err.Error(),
+			"status":  http.StatusInternalServerError,
+		})
+		return
+	}
+	fmt.Println(string(body))
+
+	c.Writer.WriteHeader(http.StatusOK)
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf(`User %s kicked from channel %s`, incomingReq.UID, channel),
+	})
+}
